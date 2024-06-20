58c29811818e73b71162c205469e64349f785236
Duong Vu <vu.t.duong01@gmail.com>
2024-06-20 09:25:35 -0400

    Add cell button click handler

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,32 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int GridSize = 3;
+        // 2D array of integers, each represent the state of a cell
+        // 0: Empty cell
+        // 1: Player 1 (X) occupied
+        // 2: Player 2 (O) occupied
+        private int[,] _grid = new int[GridSize, GridSize];
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handle player cell placement when a button on the grid is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="InvalidOperationException"></exception>
+        private void OnGridButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var tag = button.Tag.ToString() ?? throw new InvalidOperationException("Button must contain a tag");
+            int x = int.Parse(tag[0].ToString());
+            int y = int.Parse(tag[1].ToString());
+
+            button.Content = "X";
         }
     }
 }
