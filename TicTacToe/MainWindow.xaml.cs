adb0dbab66c1df78ab0438f22a9596ca68caef8c
Duong Vu <vu.t.duong01@gmail.com>
2024-06-20 10:54:37 -0400

    Add logging functionality

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,9 +38,13 @@ namespace TicTacToe
         private readonly SolidColorBrush Player1Color = Brushes.Red;
         private readonly SolidColorBrush Player2Color = Brushes.Blue;
 
+        private const string LogFileName = "tictactoelog.txt";
+
         public MainWindow()
         {
             InitializeComponent();
+            ClearFile(LogFileName);
+            LogToFile($"Game starts\n", LogFileName);
         }
 
         /// <summary>
@@ -59,17 +66,19 @@ namespace TicTacToe
             if (_grid[x - 1, y - 1] != 0)
             {
                 ShowErrorBox("Invalid cell placement!", "Error");
+                LogToFile($"Player {_currentPlayerTurn} places invalid {(_currentPlayerTurn == 1 ? Player1Symbol : Player2Symbol)} at ({x}, {y})\n", LogFileName);
                 return;
             }
 
             // Update grid array
             _grid[x - 1, y - 1] = _currentPlayerTurn;
+            LogToFile($"Player {_currentPlayerTurn} places {(_currentPlayerTurn == 1 ? Player1Symbol : Player2Symbol)} at ({x}, {y})\n", LogFileName);
 
             // Update button appearance
             button.Content = _currentPlayerTurn == 1 ? Player1Symbol : Player2Symbol;
             button.Foreground = _currentPlayerTurn == 1 ? Player1Color : Player2Color;
 
-            var winner = CheckWinner();
+            var winner = CheckWinner(_grid);
             // If no winner yet then switch turn
             if (winner == 0) SwitchTurn();
             // Else end the game
@@ -78,6 +87,9 @@ namespace TicTacToe
                 _isGameEnded = true;
                 UpdateWinnerLabel(winner);
                 restartButton.Visibility = Visibility.Visible;
+
+                if (winner == 3) LogToFile($"It's a tie!\n", LogFileName);
+                else LogToFile($"Player {winner} wins!\n", LogFileName);
             }
         }
 
@@ -107,22 +119,23 @@ namespace TicTacToe
             // Reset end game components
             winnerLabel.Visibility = Visibility.Hidden;
             restartButton.Visibility = Visibility.Hidden;
+            LogToFile("Game restarts\n", LogFileName);
         }
 
         /// <summary>
         /// Check the grid to find if there's a winner yet
         /// </summary>
         /// <returns>0 if no winner yet, 1 if player1 wins, 2 if player2 wins, 3 if it's a tie</returns>
-        private int CheckWinner()
+        private static int CheckWinner(int[,] grid)
         {
             var isGridFilled = true;
             var checkStartIndex = GridSize - 3;
             var checkEndIndex = GridSize - 1;
-            for (int x = 0; x < _grid.GetLength(0); x++)
+            for (int x = 0; x < grid.GetLength(0); x++)
             {
-                for (int y = 0; y < _grid.GetLength(1); y++)
+                for (int y = 0; y < grid.GetLength(1); y++)
                 {
-                    var cell = _grid[x, y];
+                    var cell = grid[x, y];
                     if (cell == 0)
                     {
                         isGridFilled = false;
@@ -132,23 +145,23 @@ namespace TicTacToe
                     // Check horizontal match-3
                     if (x <= checkStartIndex)
                     {
-                        if (cell == _grid[x + 1, y] && cell == _grid[x + 2, y]) return cell;
+                        if (cell == grid[x + 1, y] && cell == grid[x + 2, y]) return cell;
                     }
 
                     // Check vertical match-3
                     if (y <= checkStartIndex)
                     {
-                        if (cell == _grid[x, y + 1] && cell == _grid[x, y + 2]) return cell;
+                        if (cell == grid[x, y + 1] && cell == grid[x, y + 2]) return cell;
                     }
 
                     // Check diagonal match
                     if (x <= checkStartIndex && y <= checkStartIndex)
                     {
-                        if (cell == _grid[x + 1, y + 1] && cell == _grid[x + 2, y + 2]) return cell;
+                        if (cell == grid[x + 1, y + 1] && cell == grid[x + 2, y + 2]) return cell;
                     }
                     if (x <= checkStartIndex && y >= checkEndIndex)
                     {
-                        if (cell == _grid[x + 1, y - 1] && cell == _grid[x + 2, y - 2]) return cell;
+                        if (cell == grid[x + 1, y - 1] && cell == grid[x + 2, y - 2]) return cell;
                     }
                 }
             }
@@ -203,6 +216,28 @@ namespace TicTacToe
                     break;
             }
             winnerLabel.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Write some text to a file, create new file if file doesn't exist
+        /// </summary>
+        /// <param name="content">Text to write</param>
+        /// <param name="fileName">Name of file</param>
+        private static void LogToFile(string content, string fileName)
+        {
+            var path = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, fileName);
+            if (File.Exists(path)) File.AppendAllText(path, content);
+            else File.WriteAllText(path, content);
+        }
+
+        /// <summary>
+        /// Clear all content of a file
+        /// </summary>
+        /// <param name="fileName"></param>
+        private static void ClearFile(string fileName)
+        {
+            var path = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, fileName);
+            if (File.Exists(path)) File.WriteAllText(path, "");
         }
     }
 }
